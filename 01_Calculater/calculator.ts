1f4db7c8577620bfc09744d10a5770e69c5ce41d
Juwel-771 <juwelah99@gmail.com>
2025-10-27 16:23:40 +0600

    investment calculation login build up

--- 01_Calculater/calculator.ts
+++ 01_Calculater/calculator.ts
@@ -1,5 +1,6 @@
 type InvestmentData = {
     investmentAmount : number,
+    initialAmount: number,
     annualContribution: number,
     expectedReturn: number,
     duration: number
@@ -14,8 +15,43 @@ type InvestmentResult = {
 
 type calculateInvestmentResult = InvestmentResult [] | string;
 
-function calculateInvestment(data:InvestmentData) : calculateInvestmentResult[] {
+function calculateInvestment(data:InvestmentData) : calculateInvestmentResult {
+    const {initialAmount, annualContribution, expectedReturn, duration} = data;
+
+    if(initialAmount < 0){
+        return 'Initial investment must be at leas 0';
+    }
     
+    if(duration <=0){
+        return 'No valid amount of year are provided';
+    }
+
+    if(expectedReturn < 0){
+        return 'Expected return must be at least zero';
+    }
+
+    let total = initialAmount;
+    let totalContribution = 0;
+    let totalInterestEarning = 0;
+
+    const annualResult : InvestmentResult[] = [];
+
+    for(let i = 0; i < duration; i++){
+        total = total * (1 + expectedReturn);
+        totalInterestEarning = total - totalContribution - initialAmount;
+        totalContribution = totalContribution + annualContribution;
+        total = total+annualContribution;
+
+        annualResult.push({
+            year: `Year ${i+1}`,
+            totalAmount: total,
+            totalInterestEarning,
+            totalContribution
+        });
+    }
+
+    return annualResult;
+
 }
 
 
